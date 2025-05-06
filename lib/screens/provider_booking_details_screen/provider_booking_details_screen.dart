90ef618e7229d3304c07f9c2dedb2f7d161c8673
Irfan <irfanhpssain@gmail.com>
2025-05-06 20:03:43 +0600

    booking details for provider

--- lib/screens/provider_booking_details_screen/provider_booking_details_screen.dart
+++ lib/screens/provider_booking_details_screen/provider_booking_details_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:w_a/const/app_colors.dart';
+import 'package:w_a/const/assets_images_path.dart';
 import 'package:w_a/utils/app_size.dart';
+import 'package:w_a/utils/gap.dart';
+import 'package:w_a/widgets/app_image/app_image.dart';
 import 'package:w_a/widgets/appbar/custom_appbar.dart';
 import 'package:w_a/widgets/texts/app_text.dart';
 
@@ -17,6 +20,46 @@ class ProviderBookingDetailsScreen extends StatelessWidget {
           children: [
             SizedBox(
               width: double.infinity,
+              height: AppSize.width(value: 88),
+              //
+              child: Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(16), // 4 corner round
+                    child: AppImage(
+                      path: AssetsImagesPath.person,
+                      width: AppSize.width(value: 72),
+                      height: AppSize.width(value: 72),
+                    ),
+                  ),
+
+                  const SizedBox(width: 12),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        AppText(
+                          data: "Brooklyn Simmons",
+                          fontSize: AppSize.width(value: 20),
+                          fontWeight: FontWeight.w600,
+                          color: AppColors.subTitle,
+                        ),
+                        Gap(height: AppSize.width(value: 12)),
+                        AppText(
+                          data: "Booking iD: #54654",
+                          fontSize: AppSize.width(value: 12),
+                          fontWeight: FontWeight.w400,
+                          color: AppColors.subTitle,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
               height: AppSize.width(value: 150),
               child: Column(
                 children: [
@@ -24,7 +67,7 @@ class ProviderBookingDetailsScreen extends StatelessWidget {
                     child: Row(
                       children: [
                         Expanded(
-                          flex: 2,
+                          flex: 1,
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -65,7 +108,6 @@ class ProviderBookingDetailsScreen extends StatelessWidget {
                                 fontWeight: FontWeight.w400,
                                 color: AppColors.subTitle,
                               ),
-                              
                             ],
                           ),
                         ),
@@ -86,48 +128,48 @@ class ProviderBookingDetailsScreen extends StatelessWidget {
                           ),
                         ),
                         Expanded(
-                          flex: 4,
+                          flex: 3,
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.end,
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: [
                               AppText(
-                                data: "Basic Package",
+                                data: "#548970",
                                 fontSize: AppSize.width(value: 12),
                                 fontWeight: FontWeight.w500,
-                                color: AppColors.black400,
+                                color: AppColors.subTitle,
                               ),
                               AppText(
-                                data: "\$${210}",
+                                data: "Health & Wellness",
                                 fontSize: AppSize.width(value: 12),
                                 fontWeight: FontWeight.w500,
-                                color: AppColors.black400,
+                                color: AppColors.subTitle,
                                 maxLines: 1,
                               ),
                               AppText(
-                                data: "One Year",
+                                data: "Riverside Medical Center",
                                 fontSize: AppSize.width(value: 12),
                                 fontWeight: FontWeight.w500,
-                                color: AppColors.black400,
+                                color: AppColors.subTitle,
                               ),
                               AppText(
-                                data: "08/10/2026",
+                                data: "25 jun 2025",
                                 fontSize: AppSize.width(value: 12),
                                 fontWeight: FontWeight.w500,
-                                color: AppColors.black400,
+                                color: AppColors.subTitle,
                               ),
 
                               AppText(
-                                data: "08/12/2026",
+                                data: "10:00 am",
                                 fontSize: AppSize.width(value: 12),
                                 fontWeight: FontWeight.w500,
-                                color: AppColors.black400,
+                                color: AppColors.subTitle,
                               ),
                               AppText(
-                                data: "Expaired",
+                                data: "160",
                                 fontSize: AppSize.width(value: 12),
-                                fontWeight: FontWeight.w700,
-                                color: Colors.red,
+                                fontWeight: FontWeight.w500,
+                                color: AppColors.subTitle,
                               ),
                             ],
                           ),
